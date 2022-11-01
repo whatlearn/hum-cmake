653e85ec5503cc74bf3c224c0c60a20075b8e3eb
qi05.zhang <qi05.zhang@horizon.ai>
2022-11-01 16:47:03 +0800

    feat(smaple) add sample for whatlearn fork

---
+++ include/sample.h
@@ -0,0 +1,16 @@
+#ifndef _SAMPLE_H_
+#define _SAMPLE_H_
+
+namespace hum {
+
+class Sample {
+public:
+    Sample() = default;
+    ~Sample() = default;
+
+    static void TestSample();
+};
+
+};
+
+#endif // _SAMPLE_H_
